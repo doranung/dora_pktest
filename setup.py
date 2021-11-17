5bf5aede89568ef6d4fb0852170a4070902237a4
doranung <94506296+doranung@users.noreply.github.com>
2021-11-17 11:12:54 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 #This call to setup() does all the work
 setup(
     name="dora_pktest",
-    version="0.0.1",
+    version="1.0.0",
     author="doranung",
     author_email="",
     description="Test packages",
